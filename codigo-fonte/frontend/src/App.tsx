03c564bcbc920f00d2e231fd839d93db98f74e22
Maria Souza <84026226+MariaSouza19@users.noreply.github.com>
2026-09-19 16:56:36 -0300

    ADD AGENDA
    Agenda adicionada

--- codigo-fonte/frontend/src/App.tsx
+++ codigo-fonte/frontend/src/App.tsx
@@ -1,9 +1,27 @@
-import Dashboard from './pages/dashboard';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Dashboard, { DashboardHome } from './pages/dashboard';
+import { Agenda } from './pages/agenda';
 
 export default function App() {
   return (
-    <div>
-      <Dashboard />
-    </div>
+    <BrowserRouter>
+
+      <Routes>
+
+        {/* DASHBOARD / LAYOUT PRINCIPAL */}
+        <Route path="/" element={<Dashboard />}>
+
+          {/* Página inicial */}
+          <Route index element={<DashboardHome />} />
+
+          {/* Agenda */}
+          <Route path="agenda" element={<Agenda />} />
+
+        </Route>
+
+      </Routes>
+
+    </BrowserRouter>
   );
 }
